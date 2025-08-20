aade7efb86840615e1efc9129ed4138b9422cd83
callmedom1905 <minhtcps38149@gmail.com>
2025-08-20 23:25:10 +0700

    second commit

--- next.config.js
+++ next.config.js
@@ -5,6 +5,25 @@ const nextConfig = {
   // images: {
   //   domains: ['localhost', 'images.unsplash.com'],
   // },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   
 }
 
